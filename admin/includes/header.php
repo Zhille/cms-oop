094d1be3d12b7a97fbdb8360f1d30021fb168807
Zhille <47482982+Zhille@users.noreply.github.com>
2019-03-28 10:29:38 +0100

    init commit

---
+++ admin/includes/header.php
@@ -0,0 +1,32 @@
+<?php include("init.php"); ?>
+<?php ob_start(); ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>SB Admin - Bootstrap Admin Template</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="css/sb-admin.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+
+     <link href="css/dropzone.css" rel="stylesheet">
+     <link href="css/styles.css" rel="stylesheet">
+     <script type="text/javascript" src="https://www.google.com/jsapi"></script>
+
+</head>
+<body>
+
+    <div id="wrapper">
